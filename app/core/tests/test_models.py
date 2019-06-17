9c83824aa2d9d40fcbb7d0ef7d11347fccb827e9
Chandan Kumar <chandanchaudhary@outlook.com>
2019-06-18 01:14:54 +0530

    Normalize email to lower case before saving it Write test to check normalization of email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,14 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@GMAIL.COM'
+        password = 'Test123'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password,
+        )
+
+        self.assertEqual(user.email, email.lower())
